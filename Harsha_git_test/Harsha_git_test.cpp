bd58cdcc867fb712e56d1443694d4d3e896c45e6
Harshamaringanti48 <smaringa@aum.edu>
2026-04-08 11:15:08 -0500

    Add project files.

---
+++ Harsha_git_test/Harsha_git_test.cpp
@@ -0,0 +1,14 @@
+//Reviewed and updated by Tamora Brown
+// Edited directly on GitHub
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello from Kennedy! This is my first Git commit by Kennedy." << endl;
+    cout << "Git makes collaboration easy! " << endl;
+    cout << "Lab 1 completed on 4/8/2026!" << endl;
+    cout << "Feature by Kennedy : Branching is awesome!" << endl;
+}
+
