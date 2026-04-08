94076c592b53cb4e099f6f8782212ac3ef2884ae
Harshamaringanti48 <smaringa@aum.edu>
2026-04-08 11:44:46 -0500

    c

--- Harsha_git_test/Harsha_git_test.cpp
+++ Harsha_git_test/Harsha_git_test.cpp
@@ -6,7 +6,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello from Kennedy! This is my first Git commit by harsha" << endl;
+<<<<<<< HEAD
+    cout << "Hello from Kennedy! This is my first Git commit by harsha.." << endl;
+=======
+    cout << " This is my first Git commit by harsha" << endl;
+>>>>>>> feature/har
     cout << "Git makes collaboration easy! " << endl;
     cout << "Lab 1 completed on 4/8/2026!" << endl;
     cout << "Feature by Kennedy : Branching is awesome!" << endl;
